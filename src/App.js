0ca50c55881f5164e4727edadb3d97afffe75d75
Diasky <diaskygotah@gmail.com>
2022-06-09 14:34:10 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './tailwind/tailwind.css';
+
+function App() {
+  return (
+    <div className="container">
+      <section id='home' className='bg-[#aaa] w-screen h-screen text-[#fff]'>
+        Ciao
+      </section>
+      <section id='home' className='bg-[#bbb] w-screen h-screen text-[#fff]'>
+        Ciao
+      </section>
+      <section id='home' className='bg-[#ccc] w-screen h-screen text-[#fff]'>
+        Ciao
+      </section>
+    </div>
+  );
+}
+
+export default App;
