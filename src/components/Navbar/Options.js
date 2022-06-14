748cf4fa24cac47da5ddd4d17ee6bbbddc0465e0
Diasky <diaskygotah@gmail.com>
2022-06-14 15:49:23 +0200

    hidden on screen resize

--- src/components/Navbar/Options.js
+++ src/components/Navbar/Options.js
@@ -44,7 +44,7 @@ function Options({ btnMenu }) {
     btnMenuOutside = btnMenu
 
     return (
-        <div className="options top-[60px] fixed w-full font-roboto backdrop-blur-sm">
+        <div className="options md:hidden top-[60px] fixed w-full font-roboto backdrop-blur-sm">
             <ul id='options' className="hidden flex-col font-thin text-center bg-menuLight dark:bg-menuDark child:p-4 child:w-full child:h-full child:transition child:ease-in-out child:delay-50">
                 <li onClick = { () => document.getElementById('home').scrollIntoView({ behavior: 'smooth' }) } className='hover:bg-menuLight dark:hover:bg-menuDark'>Home</li>
                 <li onClick = { () => document.getElementById('works').scrollIntoView({ behavior: 'smooth' }) } className='hover:bg-menuLight dark:hover:bg-menuDark'>Works</li>
