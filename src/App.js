f95a1fac7c101bfd65a555eae16eedf16ff0a9fc
Diasky <diaskygotah@gmail.com>
2022-06-21 23:09:03 +0200

    transition + btn

--- src/App.js
+++ src/App.js
@@ -13,6 +13,13 @@ import useNight from './hooks/useNight'
 // CSS
 import './css/scrollbar.css'
 
+// Icons
+import { ReactComponent as Download } from './icons/download.svg'
+
+// CVS
+import darkcv from './imgs/cvs/darkcv.png'
+import lightcv from './imgs/cvs/lightcv.png'
+
 function App() {
 
     const [isNight, setIsNight] = useNight()
@@ -23,9 +30,19 @@ function App() {
             <div className="mobile-divider h-[60px] md:hidden"></div>
             <section id='home' className='bg-bgLight dark:bg-bgDark w-screen h-screen'>
                 <section className='h-[calc(100vh-60px)] md:h-[calc(100vh-120px)] flex items-center justify-center'>
-                    <div className='text-3xl md:text-5xl font-roboto font-thin text-center'>
-                        <p>Hello, I'm Camillo.</p>
-                        <p>I'm a <a className='font-normal'>Dream Aviator</a>.</p>
+                    <div className='text-3xl md:text-5xl font-roboto font-thin text-center mb-10 space-y-5'>
+                        <div className="welcome">
+                            <p>Hello, I'm Camillo.</p>
+                            <p>I'm a <a className='font-normal'>Dream Aviator</a>.</p>
+                        </div>
+                        <a download = { isNight ? 'camillo-darkcv' : 'camillo-lightcv' } href = { isNight ? darkcv : lightcv } className="block text-xl p-2 text-white font-normal bg-blurple border-2 border-bgLight dark:border-bgDark rounded-md shadow-[0_0_0_2px_var(--blurple)]">
+                            <div className="btnContent flex justify-center items-center">
+                                <div className="download-icon w-[25px] mr-2">
+                                    <Download fill = '#fff' />
+                                </div>
+                                Download CV
+                            </div>
+                        </a>
                     </div>
                 </section>
             </section>
