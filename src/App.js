b6d95121f310c5ed1a59c975aef766af97e48436
Diasky <diaskygotah@gmail.com>
2022-06-21 22:20:08 +0200

    dream aviator

--- src/App.js
+++ src/App.js
@@ -22,7 +22,12 @@ function App() {
             <ThemeToggler isNight = { isNight } setIsNight = { setIsNight } />
             <div className="mobile-divider h-[60px] md:hidden"></div>
             <section id='home' className='bg-bgLight dark:bg-bgDark w-screen h-screen'>
-                ciao
+                <section className='h-[calc(100vh-60px)] md:h-[calc(100vh-120px)] flex items-center justify-center'>
+                    <div className='text-3xl md:text-5xl font-roboto font-thin text-center'>
+                        <p>Hello, I'm Camillo.</p>
+                        <p>I'm a <a className='font-normal'>Dream Aviator</a>.</p>
+                    </div>
+                </section>
             </section>
             <section id='works' className='bg-bgLight dark:bg-bgDark w-screen h-screen'>
                 Ciao
