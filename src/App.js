3acc8d5e649c6d6ba8b20fc7815c2fa05bf0eb38
Diasky <diaskygotah@gmail.com>
2022-06-23 08:41:05 +0200

    plane animation

--- src/App.js
+++ src/App.js
@@ -24,20 +24,54 @@ import lightcv from './imgs/cvs/lightcv.png'
 function App() {
 
     const [isNight, setIsNight] = useNight()
+    const [isFlying, setIsFlying] = useState(false)
+
+    useEffect(() => {
+        const plane = document.getElementById('plane')
+        const animatePlane = () => {
+            if (isFlying) return null
+            setIsFlying(true)
+            plane.animate([
+                { transform: 'translate(0, 0)' },
+                { transform: 'translate(30px, -30px)' }
+            ], { duration: 1E3 })
+            plane.animate([
+                { opacity: 0 },
+                { opacity: 1 },
+            ], { duration: 500 })
+            plane.style.opacity = 1
+            setTimeout(
+                () => {
+                    plane.animate([
+                        { opacity: 1 },
+                        { opacity: 0 },
+                    ], { duration: 200 })
+                    setTimeout(() => {
+                        plane.style.opacity = 0
+                        setTimeout(() => setIsFlying(false), 300)
+                    }, 200)
+                },
+                800
+            )
+        }
+        const dreamAviator = document.getElementById('dreamaviator')
+        dreamAviator.addEventListener('mouseenter', animatePlane)
+        return () => dreamAviator.removeEventListener('mouseenter', animatePlane)
+    })
 
     return (
         <div className="container w-screen h-full">
             <ThemeToggler isNight = { isNight } setIsNight = { setIsNight } />
             <div className="mobile-divider h-[60px] md:hidden"></div>
             <section id='home' className='bg-bgLight dark:bg-bgDark w-screen h-screen'>
-                <div className="plane-animation absolute top-[450px] right-[350px] hidden">
-                    <Plane fill = { isNight ? '#fff' : '#000' } className = '-rotate-45 w-[30px]'/>
-                </div>
                 <section className='h-[calc(100vh-60px)] md:h-[calc(100vh-120px)] flex items-center justify-center'>
                     <div className='text-3xl md:text-5xl font-roboto font-thin text-center mb-10 space-y-5'>
-                        <div className="welcome">
+                        <div className="welcome relative cursor-default z-0">
                             <p>Hello, I'm Camillo.</p>
-                            <p>I'm a <a className='font-normal'>Dream Aviator</a>.</p>
+                            <p>I'm a <a id='dreamaviator' className='font-normal'>Dream Aviator</a>.</p>
+                            <div id='plane' className="plane-animation absolute top-[10px] md:top-[20px] right-0 opacity-0">
+                                <Plane fill = { isNight ? '#fff' : '#000' } className = '-rotate-45 w-[30px]'/>
+                            </div>
                         </div>
                         <a download = { isNight ? 'camillo-darkcv' : 'camillo-lightcv' } href = { isNight ? darkcv : lightcv } className="block text-xl p-2 text-white font-normal bg-blurple border-2 border-bgLight dark:border-bgDark rounded-md shadow-[0_0_0_2px_var(--blurple)]">
                             <div className="btnContent flex justify-center items-center">
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -16,8 +16,8 @@ function Navbar() {
     const [isNight, setIsNight] = useNight(false)
 
     return (
-        <div className = "md:sticky md:top-0 text-black dark:text-white">
-            <nav className = 'fixed md:static w-screen bg-menuLight dark:bg-menuDark backdrop-blur-sm font-roboto z-10'>
+        <div className = "md:sticky md:top-0 text-black dark:text-white z-10">
+            <nav className = 'fixed md:static w-screen bg-menuLight dark:bg-menuDark backdrop-blur-md font-roboto'>
                 <div className = 'flex md:block w-full max-w-xl mx-auto p-4 items-center md:items-end justify-around text-center'>
                     <div className="logo md:hidden w-screen font-roboto z-10">
                         <a href = "#" className = 'text-xl md:text-5xl'>Camillo <span className = "font-thin">Portfolio</span></a>
