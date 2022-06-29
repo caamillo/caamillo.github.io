d13865a213fb5650281b471931a5edc08eacb5f1
Diasky <diaskygotah@gmail.com>
2022-06-29 22:10:34 +0200

    working on pages

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import './css/scrollbar.css'
 // Icons
 import { ReactComponent as Download } from './icons/download.svg'
 import { ReactComponent as Plane } from './icons/plane.svg'
+import { ReactComponent as Phone } from './icons/phone.svg'
 
 // CVS
 import darkcv from './imgs/cvs/darkcv.png'
@@ -82,11 +83,33 @@ function App() {
                     </div>
                 </section>
             </section>
-            <section id='works' className='bg-bgLight dark:bg-bgDark w-screen h-screen'>
-                Coming Soon...!
+            <section id='works' className='bg-bgLight dark:bg-bgDark w-screen h-full md:h-screen'>
+                <h1 className='text-center text-3xl p-10'>Latest Works</h1>
+                <div className="wrapper flex flex-row justify-center">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                        <div className='w-[200px] h-[200px] md:w-[300px] md:h-[300px] border-2 border-blurple rounded-md'>ciao</div>
+                        <div className='w-[200px] h-[200px] md:w-[300px] md:h-[300px] border-2 border-blurple rounded-md'>ciao</div>
+                        <div className='w-[200px] h-[200px] md:w-[300px] md:h-[300px] border-2 border-blurple rounded-md'>ciao</div>
+                        <div className='w-[200px] h-[200px] md:w-[300px] md:h-[300px] border-2 border-blurple rounded-md'>ciao</div>
+                    </div>
+                </div>
             </section>
-            <section id='contact' className='bg-bgLight dark:bg-bgDark w-screen h-screen'>
-                Coming Soon...!
+            <section id='contact' className='bg-bgLight dark:bg-bgDark w-screen h-full'>
+                <div className="contact-container flex flex-col justify-center items-center h-[calc(100vh-2.5rem)]">
+                    <h1 className='text-2xl md:text-3xl mb-3'>Feel free to contact me</h1>
+                    <div className="mobile-container flex justify-center items-center h-[50px] rounded-md shadow-[0_0_0_2px_var(--blurple)]">
+                        <div className="icon w-[50px] h-[50px] flex items-center justify-center text-bgDark bg-blurple border-2 border-bgLight dark:border-bgDark rounded-md shadow-[0_0_0_2px_var(--blurple)]">
+                            <div className="icon w-[20px]">
+                                <Phone fill='#fff' />
+                            </div>
+                        </div>
+                        <div className="contact-id px-3">+39 329 634 0655</div>
+                    </div>
+                    <div className="text text-sm mt-4">or send me an <a href="mailto:mcamillolud@gmail.com" className='text-blurple'>email</a></div>
+                </div>
+                <div className="social-container relative">
+                        <div className="social-media-icons absolute left-0 right-0 mx-auto bottom-[25px] w-[100px] text-center">socials</div>
+                    </div>
             </section>
         </div>
     );
