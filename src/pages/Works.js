7e2833d9d4e326344e1639d9858843d40412409f
Diasky <diaskygotah@gmail.com>
2022-07-05 23:46:00 +0200

    lang

--- src/pages/Works.js
+++ src/pages/Works.js
@@ -2,13 +2,16 @@
 import { Link, useParams } from 'react-router-dom'
 
 // Lang
+import works from '../lang/works.json'
 
 
 function Works() {
     const { workName } = useParams()
+    console.log(works)
     return (
         <div className="wrapper h-[calc(100vh-60px)] md:h-[calc(100vh-120px)]">
-            { workName }
+            <h1>{ works['en']['title'] }</h1>
+            <h2>{ works['en']['desc'] }</h2>
         </div>
     );
 }
